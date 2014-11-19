3217491fa216effffe4eb6271b66f936747932dd
Joe <gorsejoe@gmail.com>
2014-11-19 11:17:38 -0500

    Added instructor's code. Began outlining graph function

---
+++ gorwal5/gorwal5/p5.cpp
@@ -0,0 +1,182 @@
+// Project 5
+
+#include <iostream>
+#include <limits.h>
+#include "d_except.h"
+#include <list>
+#include <fstream>
+#include "d_matrix.h"
+#include "graph.h"
+
+using namespace std;
+
+class maze
+{
+   public:
+      maze(ifstream &fin);
+      void print(int,int,int,int);
+      bool isLegal(int i, int j);
+
+      void setMap(int i, int j, int n);
+      int getMap(int i, int j) const;
+      void mapMazeToGraph(graph &g);
+
+   private:
+      int rows; // number of rows in the maze
+      int cols; // number of columns in the maze
+
+      matrix<bool> value;
+      matrix<int> map;      // Mapping from maze (i,j) values to node index values
+};
+
+void maze::setMap(int i, int j, int n)
+// Set mapping from maze cell (i,j) to graph node n. 
+{
+}
+
+int maze ::getMap(int i, int j) const
+// Return mapping of maze cell (i,j) in the graph.
+{
+	return 0;
+}
+
+maze::maze(ifstream &fin)
+// Initializes a maze by reading values from fin.  Assumes that the
+// number of rows and columns indicated in the file are correct.
+{
+   fin >> rows;
+   fin >> cols;
+
+   char x;
+
+   value.resize(rows,cols);
+   for (int i = 0; i <= rows-1; i++)
+      for (int j = 0; j <= cols-1; j++)
+      {
+	 fin >> x;
+	 if (x == 'O')
+            value[i][j] = true;
+	 else
+	    value[i][j] = false;
+      }
+
+   map.resize(rows,cols);
+}
+
+void maze::print(int goalI, int goalJ, int currI, int currJ)
+// Print out a maze, with the goal and current cells marked on the
+// board.
+{
+   cout << endl;
+
+   if (goalI < 0 || goalI > rows || goalJ < 0 || goalJ > cols)
+      throw rangeError("Bad value in maze::print");
+
+   if (currI < 0 || currI > rows || currJ < 0 || currJ > cols)
+      throw rangeError("Bad value in maze::print");
+
+   for (int i = 0; i <= rows-1; i++)
+   {
+      for (int j = 0; j <= cols-1; j++)
+      {
+	 if (i == goalI && j == goalJ)
+	    cout << "*";
+	 else
+	    if (i == currI && j == currJ)
+	       cout << "+";
+	    else
+	       if (value[i][j])
+		  cout << " ";
+	       else
+		  cout << "X";	  
+      }
+      cout << endl;
+   }
+   cout << endl;
+}
+
+bool maze::isLegal(int i, int j)
+// Return the value stored at the (i,j) entry in the maze.
+{
+   if (i < 0 || i > rows || j < 0 || j > cols)
+      throw rangeError("Bad value in maze::isLegal");
+
+   return value[i][j];
+}
+
+void maze::mapMazeToGraph(graph &g)
+// Create a graph g that represents the legal moves in the maze m.
+{
+	//already have a graph, g, of the correct size.
+	
+	for(int i =0; i<row; i++)
+	{
+		for(int j=0; j<col; j++)
+		{
+			//Up, (i-1, j)
+			if(i != 0)
+				if(isLegal(i-1, j)
+					//Create edge
+
+			//Down (i+1, j)
+			if(i != row-1);
+				if(isLegal(i+1, j);
+					//Create edge
+
+			//left (i, j-1)
+			if(j != 0)
+				if(isLegal(i, j-1)
+					//create edge
+
+			//right (i, j+1)
+			if(j != col-1)
+				if(isLegal(i, j+1)
+					//create edge
+
+		}
+	}
+	
+	//for each cell in maze
+		//for each adjacent cell
+			//if legal move, add edge in graph
+	
+}
+
+int main()
+{
+   char x;
+   ifstream fin;
+   
+   // Read the maze from the file.
+   string fileName = "maze1.txt";
+
+   fin.open(fileName.c_str());
+   if (!fin)
+   {
+      cerr << "Cannot open " << fileName << endl;
+      exit(1);
+   }
+
+   try
+   {
+
+      graph g;
+      while (fin && fin.peek() != 'Z')
+      {
+         maze m(fin);
+		 m.print(7, 10, 0, 0);
+      }
+
+
+   } 
+   catch (indexRangeError &ex) 
+   { 
+      cout << ex.what() << endl; exit(1);
+   }
+   catch (rangeError &ex)
+   {
+      cout << ex.what() << endl; exit(1);
+   }
+
+   system("pause");
+}
